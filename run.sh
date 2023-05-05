ed9833dfe48ea48a1861f7a92599a3dd4c3ade94
Nikita Korotkov <n.korotkov@g.nsu.ru>
2023-05-05 12:14:00 +0700

    Fixed shell scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./$1 corpus/articles dictionaries/dict.opcorpora.xml dictionaries/freqrnc2011.csv
+./$1 corpus/articles dictionaries/dict.opcorpora.xml dictionaries/freqrnc2011.csv "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
